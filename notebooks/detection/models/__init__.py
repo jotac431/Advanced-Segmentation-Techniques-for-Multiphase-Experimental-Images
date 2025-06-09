94475c7e86152b99218202ae3de6c9a423c1edc9
Jorge Costa <48180944+jotac431@users.noreply.github.com>
2025-06-09 16:44:47 +0100

    Model Testing, augmentations, logs, configs

--- notebooks/detection/models/__init__.py
+++ notebooks/detection/models/__init__.py
@@ -1,8 +1,10 @@
 # models/__init__.py
-from .maskrcnn import get_maskrcnn_model
+from .maskrcnn import get_maskrcnn_resnet50_fpn, get_maskrcnn_resnet101_fpn, get_maskrcnn_mobilenet
 from .deeplabv3 import get_deeplab_model
 
 MODEL_REGISTRY = {
-    "maskrcnn_resnet50_fpn": get_maskrcnn_model,
+    "maskrcnn_resnet50_fpn": get_maskrcnn_resnet50_fpn,
+    "maskrcnn_resnet101_fpn": get_maskrcnn_resnet101_fpn,
+    "maskrcnn_mobilenet": get_maskrcnn_mobilenet,
     "deeplabv3_resnet50": get_deeplab_model
 }
--- notebooks/detection/models/maskrcnn.py
+++ notebooks/detection/models/maskrcnn.py
@@ -3,23 +3,34 @@ from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
 
 
-def get_maskrcnn_model(num_classes):
+def get_maskrcnn_resnet50_fpn(num_classes):
     # load an instance segmentation model pre-trained on COCO
     model = torchvision.models.detection.maskrcnn_resnet50_fpn(weights="DEFAULT")
+    return _replace_heads(model, num_classes)
+
+def get_maskrcnn_resnet101_fpn(num_classes):
+    model = torchvision.models.detection.maskrcnn_resnet101_fpn(weights="DEFAULT")
+    return _replace_heads(model, num_classes)
+
+def get_maskrcnn_mobilenet(num_classes):
+    model = torchvision.models.detection.maskrcnn_mobilenet_v3_large_fpn(weights="DEFAULT")
+    return _replace_heads(model, num_classes)
+
+def _replace_heads(model, num_classes):
 
     # get number of input features for the classifier
     in_features = model.roi_heads.box_predictor.cls_score.in_features
+
     # replace the pre-trained head with a new one
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
     # now get the number of input features for the mask classifier
     in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
     hidden_layer = 256
+
     # and replace the mask predictor with a new one
     model.roi_heads.mask_predictor = MaskRCNNPredictor(
-        in_features_mask,
-        hidden_layer,
-        num_classes
+        in_features_mask, hidden_layer, num_classes
     )
 
     return model
